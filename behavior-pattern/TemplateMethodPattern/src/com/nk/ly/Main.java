a07dfcaa6b15fad1b1da4feebadd3bf5938af1d3
Naing Lynn Aung <133348339+nainglynnaung-dev@users.noreply.github.com>
2026-03-10 14:03:54 +0630

    template method -pattern

---
+++ behavior-pattern/TemplateMethodPattern/src/com/nk/ly/Main.java
@@ -0,0 +1,9 @@
+package com.nk.ly;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Template Method Pattern");
+	}
+
+}
---
+++ behavior-pattern/TemplateMethodPattern/src/com/nk/ly/NewYorkPizza.java
@@ -0,0 +1,29 @@
+package com.nk.ly;
+
+public class NewYorkPizza extends PizzaTemplate{
+
+	@Override
+	public void prepared() {
+		System.out.println(this.getClass().getSimpleName() + "prepared");
+		
+	}
+
+	@Override
+	public void baked() {
+		System.out.println(this.getClass().getSimpleName() + "baked");
+		
+	}
+
+	@Override
+	public void topping() {
+		System.out.println(this.getClass().getSimpleName() + "topping");
+		
+	}
+
+	@Override
+	public void served() {
+		System.out.println(this.getClass().getSimpleName() + "served");
+		
+	}
+
+}
